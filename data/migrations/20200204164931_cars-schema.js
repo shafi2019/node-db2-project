ae767a650a931bf7b250d597450add20f9cc5832
Shafi Masoumi <masoumi2017@icloud.com>
2020-02-04 17:04:09 -0800

    Cars Schema created with its table

---
+++ data/migrations/20200204164931_cars-schema.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl =>{
+      tbl.increments();
+      tbl.text('Make', 128).unique().notNullable();
+      tbl.text('Model', 128).unique().notNullable();
+      tbl.decimal('year');   
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
